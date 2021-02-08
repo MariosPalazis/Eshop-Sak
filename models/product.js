eaeaeec22d12ca614e67db1bf361e6bd1d1d6caf
pavlos noulis <pavlos.noulis@gmail.com>
2021-02-08 09:32:50 +0000

    pavlos quick merge

--- models/product.js
+++ models/product.js
@@ -1,32 +1,88 @@
 import mongoose from "mongoose";
 
+// const productSchema = new mongoose.Schema({
+//   productID: {type: String, index: true},
+//   description: String,
+//   stock: Number,
+//   price: {
+//     amount: Number,
+//     unit: String,
+//   },
+//   shipping_price: {
+//     amount: Number,
+//     unit: String,
+//   },
+//   wheel_diameter: {
+//     amount: Number,
+//     unit: String,
+//   },
+//   thickness: {
+//     amount: Number,
+//     unit: String,
+//   },
+//   leather_color: String,
+//   thread_color: String,
+//   spokes: String,
+//   color_of_spokes: String,
+// })
+
+
 const productSchema = new mongoose.Schema({
   productID: {type: String, index: true},
   description: String,
-  stock: Number,
-  price: {
-    amount: Number,
-    unit: String,
-  },
-  shipping_price: {
-    amount: Number,
-    unit: String,
-  },
-  wheel_diameter: {
-    amount: Number,
-    unit: String,
-  },
-  thickness: {
-    amount: Number,
-    unit: String,
-  },
-  leather_color: String,
-  thread_color: String,
-  spokes: String,
-  color_of_spokes: String,
+  wheelDiameter: [
+    {
+      amount: [Number],
+      unit: String,
+      price: Number,
+      priceUnit: String,
+      stock: Number,
+    }
+  ],
+  thickness: [
+    {
+      amount: Number,
+      unit: String,
+      price: Number,
+      priceUnit: String,
+      stock: Number,
+    }
+  ],
+  leatherColor: [
+    {
+      color: String,
+      price: Number,
+      priceUnit: String,
+      stock: Number,
+    }
+  ],
+  threadColor: [
+    {
+      color: String,
+      price: Number,
+      priceUnit: String,
+      stock: Number,
+    }
+  ],
+  spokes: [
+    {
+      description: String,
+      amount: Number,
+      unit: String,
+      price: Number,
+      priceUnit: String,
+      stock: Number,
+    }
+  ],
+  colorOfSpokes: [
+    {
+      color: String,
+      price: Number,
+      priceUnit: String,
+      stock: Number,
+    }
+  ],
 })
-
-
 
 const Product = mongoose.model("Product", productSchema);
 
