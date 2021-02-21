c32bb1b02ae70c7db5fee0f3edce6b842ea818f0
MariosPalazis <56583171+MariosPalazis@users.noreply.github.com>
2021-02-21 13:20:45 +0200

    delete config

--- config/config.js
+++
@@ -1,9 +0,0 @@
-const env = process.env.NODE_ENV || "development";
-const port = process.env.PORT || 3000;
-import credentials from "./.credentials.development.js";
-
-export default {
-  env: env,
-  port: port,
-  credentials: credentials
-};
